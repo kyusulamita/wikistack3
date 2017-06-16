80adbe5926a76f7c8ac1c7c3146cd3b2daad1b82
kyusulamita <sulamitamorales@yahoo.com>
2017-06-16 15:39:28 -0400

    why won't you be nice

--- models/index.js
+++ models/index.js
@@ -3,6 +3,15 @@ var db = new Sequelize('postgres://localhost/wikistack', {
 	logging: false
 });
 
+
+// function generateUrlTitle(title){
+//     if(title){
+//         return title.replace(/\s+/g, '_').replace(/\W/g, '');
+//     }else{
+//         return Math.random().toString(36).substring(2,7);
+//     }
+// };
+
 const Page = db.define('page', {
     title: {
         type: Sequelize.STRING,
@@ -29,7 +38,21 @@ const Page = db.define('page', {
 			return '/wiki/' + this.urlTitle;
 		}
 	}
-});
+},
+    {
+        hooks: {
+            beforeValidate:(page, options)=>{
+                console.log("Hook firing");
+                // page.urlTitle = generateUrlTitle("hamster");
+                if(page.title){
+                    page.urlTitle = title.replace(/\s+/g, '_').replace(/\W/g, '');
+                }else{
+                    page.urlTitle =  Math.random().toString(36).substring(2,7);
+                }
+            }
+        }
+    }
+);
 
 const User = db.define('user', {
     name: {
