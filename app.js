216f936b7dae773b9579d580e1bb55a4f628a651
dbui0051 <dennisd.bui@gmail.com>
2017-06-16 11:37:41 -0400

    setup wiki

---
+++ app.js
@@ -0,0 +1,8 @@
+const nunjucks = require('nunjucks')
+
+
+var env = nunjucks.configure('views', {noCache: true});
+// have res.render work with html files
+app.set('view engine', 'html');
+// when res.render works with html files, have it use nunjucks to do so
+app.engine('html', nunjucks.render);
